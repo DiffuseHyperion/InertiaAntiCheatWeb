3f4e153dd49ff4d660cc837b594e7be19fcf2b01
DiffuseHyperion <95083897+DiffuseHyperion@users.noreply.github.com>
2024-01-16 20:37:11 +0800

    add a bit of margin to select file button text

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,7 @@ function App() {
           </div>
           <div className="w-full flex flex-col space-y-4 mt-8">
             <div className={"h-20 w-full relative bg-white rounded-2xl flex items-center justify-center"}>
-              <p className={"absolute text-black text-center"}>Click to select mods / Drag mods here!</p>
+              <p className={"m-4 absolute text-black text-center"}>Click to select mods / Drag mods here!</p>
               <input className="w-full h-full opacity-0 z-10" type="file" multiple={true} accept={"*,.jar"}
                      id="files" name="files"
                      onChange={() => updateMods()}/>
